9c28bde3825b021c12140a1d9ec90d070f128cfe
Sheldon <sheldonc@iastate.edu>
2024-12-10 15:04:12 -0600

    Basic backend setup

---
+++ Backend/App.js
@@ -0,0 +1,30 @@
+import express, { json } from "express";
+import cors from "cors";
+import fs from "fs";
+var app = express();
+app.use(cors());
+app.use(json());
+
+const { MongoClient } = require("mongodb");
+
+const port = "8081";
+const host = "localhost";
+app.listen(port, () => {
+  console.log("App listening at http://%s:%s", host, port);
+});
+
+// MongoDB constants
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "voyage_viewer";
+const client = new MongoClient(url);
+const db = client.db(dbName);
+
+// GET All
+app.get("/destination", async (req, res) => {
+    await client.connect();
+    const query = {};
+    const results = await db.collection("destination").find(query).limit(100).toArray();
+  
+    if (!results) res.status(404);
+    else res.send(results).status(200);
+});
