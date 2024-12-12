4f718198dd96db696cab78e56c9281bb90edac39
Kennedy Wendl <kmwendl@iastate.edu>
2024-12-11 22:03:33 -0600

    added changes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,6 @@ import '../App.css'
 
 function Home() {
   const handleRandomCity = () => {
-    // Implement random city selection logic here
     console.log('Random city button clicked');
   };
 
@@ -12,10 +11,7 @@ function Home() {
       <div className="bg-overlay">
         <div className="bg-text text-center">
           <h1 className="display-3">Welcome to Voyage Viewer!</h1>
-          <p className="text-wrap">Here you can find where you want to go for your next vacation.</p>
-          <button className="button" onClick={handleRandomCity}>
-            Choose a city for me!
-          </button>
+          <p className="text-wrap">Here you can find where you want to go for your next vacation</p>
         </div>
       </div>
     </div>
